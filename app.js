12a9eb69c20980c67511b9fbc3ff9c45f6dd8218
Diego Araujo <127630045+devdiegoSE@users.noreply.github.com>
2024-09-07 13:34:02 -0300

    melhorei busca

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+       //Se campoPesquisa for uma string sem nada
+    if (campoPesquisa == "") {
+        section.innerHTML = "Nada foi encontrado";
+        return
+    }
+
+    //transforma em letra minuscula
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados formatados em HTML
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada objeto de dado (resultado da pesquisa)
     for (let dado of dados) {
-        // Constrói o HTML para cada resultado, formatando o título, descrição e link
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //Se titulo includes campoPesquisa true
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){ 
+            // Cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -18,6 +37,7 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
     }
 
     // Atualiza o conteúdo HTML da seção com os resultados formatados
--- dados.js
+++ dados.js
@@ -1,20 +1,40 @@
 let dados = [ 
     {
-    titulo: "Lewis Hamilton",
-    descricao: "Lewis Carl Davidson Hamilton é um piloto de Fórmula 1 britânico, sete vezes campeão mundial (2008, 2014, 2015, 2017, 2018, 2019 e 2020). Hamilton é o piloto com mais vitórias, poles e pódios na história da Fórmula 1. Conhecido por sua velocidade, habilidade em ultrapassagens e ativismo social.",
-    link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton"
-},
+        titulo: "Lewis Hamilton",
+        descricao: "Piloto britânico de Fórmula 1, heptacampeão mundial, conhecido por sua velocidade, ultrapassagens e ativismo. É considerado um dos maiores pilotos da história da Fórmula 1 e o primeiro piloto negro a conquistar um título mundial na categoria.",
+        tags: "sir",
+        link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+        nacionalidade: "Britânica",
+        equipes: "McLaren, Mercedes",
+        titulos: 7,
+        vitorias: 103,
+        poles: 103,
+        podios: 192
+    },
 
     {
-    titulo: "Ayrton Senna",
-    descricao: "Ayrton Senna da Silva foi um piloto de Fórmula 1 brasileiro, tricampeão mundial (1988, 1990 e 1991). Considerado um dos maiores pilotos de todos os tempos, Senna era conhecido por sua paixão, habilidade e determinação. Sua morte trágica em 1994 chocou o mundo do automobilismo.",
-    link: "https://pt.wikipedia.org/wiki/Ayrton_Senna"
-},
+        titulo: "Ayrton Senna da Silva",
+        descricao: "Piloto de Fórmula 1 brasileiro, tricampeão mundial (1988, 1990 e 1991), conhecido por sua habilidade, velocidade e carisma. Considerado um dos maiores pilotos de todos os tempos, Senna deixou um legado marcante no esporte e no coração dos fãs.",
+        tags: "brasil rei monaco Chuva",
+        link: "https://pt.wikipedia.org/wiki/Ayrton_Senna",
+        nacionalidade: "Brasileira",
+        equipes: "Toleman, Lotus, McLaren, Williams",
+        titulos: 3,
+        vitorias: 41,
+        poles: 65,
+        podios: 80
+    },
 
     {
-    titulo : "Michael Schumacher",
-    descricao: "Michael Schumacher é um ex-piloto alemão de Fórmula 1, considerado um dos maiores de todos os tempos. Conquistou sete títulos mundiais de pilotos, um recorde que perdurou por mais de uma década. É conhecido por sua consistência e habilidade em ultrapassagens.",
-    link : "https://pt.wikipedia.org/wiki/Michael_Schumacher"
-    }
-
+        titulo: "Michael Schumacher",
+        descricao: "Piloto alemão de Fórmula 1, heptacampeão mundial, conhecido por sua consistência, agressividade e domínio técnico. É considerado um dos maiores pilotos da história da Fórmula 1.",
+        tags: "schumi melhor",
+        link: "https://pt.wikipedia.org/wiki/Michael_Schumacher",
+        nacionalidade: "Alemanha",
+        equipes: "Jordan, Benetton, Ferrari, Mercedes",
+        titulos: 7,
+        vitorias: 91,
+        poles: 68,
+        podios: 155
+    },
 ];    /*Uma lista[] com varios objetos{}*/
